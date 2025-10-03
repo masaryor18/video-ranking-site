35d443de30ba6cacbbd3f95d53935f550f90f9ab
masaryor18 <rainbow7happy23@gmail.com>
2025-10-03 20:44:10 +0900

    ads.jsを追加

---
+++ src/ads.js
@@ -0,0 +1,6 @@
+export const bannerAd = `
+  <a href="https://al.fanza.co.jp/?lurl=https%3A..." rel="sponsored" target="_blank">
+    <img src="https://doujin-assets.dmm.co.jp/digital/comic/d_664981/d_664981pl.jpg" 
+         style="margin:0;padding:0;border:0;max-width:100%;" alt="広告バナー" />
+  </a>
+`;
