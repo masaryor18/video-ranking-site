b38013cb95ac67bf52841e8e864c180fe8a6b30a
masaryor18 <rainbow7happy23@gmail.com>
2025-10-27 23:18:50 +0900

    chore: separate build for index.html and admin.html

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,20 @@
 // vite.config.js
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 
 export default defineConfig({
   root: './',
   build: {
-    outDir: 'dist'
+    outDir: 'dist',
+    rollupOptions: {
+      input: {
+        // ページごとに明示的にエントリを指定
+        main: resolve(__dirname, 'index.html'),
+        admin: resolve(__dirname, 'admin.html')
+      }
+    }
+  },
+  server: {
+    open: true
   }
 })
